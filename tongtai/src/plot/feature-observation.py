6eae10ab6e46d5c73ec428af09db740ad2961208
翁治平 <gocreating@gmail.com>
2017-03-13 02:04:39 +0800

    Add feature observation plot for tongtai data

---
+++ tongtai/src/plot/feature-observation.py
@@ -0,0 +1,174 @@
+# coding=UTF-8
+import sys
+import logging
+import glob
+import os
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+SRC_DIR = u'../../build/data/labeled/第五軸 ABL TDA 168000轉 1267孔'
+DEST_DIR = u'../../build/plots/feature-observation/第五軸 ABL TDA 168000轉 1267孔'
+INPUT_CSV_COLUMNS = [
+    'yyyy', 'MM', 'dd', 'hh', 'mm', 'ss', 'fff',
+    'x', 'y', 'z',
+    'uInG', 'vInG', 'wInG',
+    'rul',
+]
+CONFIG_MAP = {
+    'default': {
+        'marker': {
+            'linewidth': 0,
+            'marker': '.',
+            'markersize': 1,
+            'color': 'blue',
+            'alpha': 0.5,
+            'zorder': 10,
+        },
+        # 'ylim': [-0.8, 0.8],
+    },
+    'x': {
+        'marker': {
+            'color': 'red',
+        },
+        'ylim': [-5000, 5000],
+        'label': 'Accelerometer Reading (??)',
+    },
+    'y': {
+        'marker': {
+            'color': 'green',
+        },
+        'ylim': [-5000, 5000],
+        'label': 'Accelerometer Reading (??)',
+    },
+    'z': {
+        'marker': {
+            'color': 'blue',
+        },
+        'ylim': [-5000, 5000],
+        'label': 'Accelerometer Reading (??)',
+    },
+    'uInG': {
+        'marker': {
+            'color': 'red',
+        },
+        'ylim': [-0.03, 0.03],
+        'label': 'U Phase Reading (g)',
+    },
+    'vInG': {
+        'marker': {
+            'color': 'green',
+        },
+        'ylim': [-0.03, 0.03],
+        'label': 'V Phase Reading (g)',
+    },
+    'wInG': {
+        'marker': {
+            'color': 'blue',
+        },
+        'ylim': [-0.03, 0.03],
+        'label': 'W Phase Reading (g)',
+    },
+}
+FEATURE_PLOTS = {
+    'M1': ['x'],
+    'M2': ['y'],
+    'M3': ['z'],
+    'M4': ['x', 'y', 'z'],
+    'M5': ['uInG', 'vInG', 'wInG'],
+}
+
+def toMillisecond(rowOrDataframe):
+    return (
+        (
+            rowOrDataframe['hh'] * 3600 +
+            rowOrDataframe['mm'] * 60 +
+            rowOrDataframe['ss']
+        ) * 1000 +
+        rowOrDataframe['fff']
+    )
+
+def plot(df, filename):
+    for featurePlot in FEATURE_PLOTS:
+        featureNames = FEATURE_PLOTS[featurePlot]
+        lines = ()
+        labels = ()
+        axisOffset = 1
+        fig, ax = plt.subplots()
+        ax.yaxis.set_visible(False)
+        ax.patch.set_visible(False)
+        ax.set_xlabel('Time (ms)')
+
+        for featureName in featureNames:
+            axFeature = ax.twinx()
+            marker = dict(
+                CONFIG_MAP['default']['marker'].items() +
+                CONFIG_MAP[featureName]['marker'].items()
+            )
+            configs = dict(
+                CONFIG_MAP['default'].items() +
+                CONFIG_MAP[featureName].items()
+            )
+
+            new_fixed_axis = axFeature.spines['right'].set_position(('axes', axisOffset))
+            axisOffset += 0.2
+            axFeature.yaxis.tick_right()
+            axFeature.set_xlim(
+                df['millisecond'].min(),
+                df['millisecond'].max() + (
+                    (
+                        df['millisecond'].max() -
+                        df['millisecond'].min()
+                    ) *
+                    0.1
+                )
+            )
+            axFeature.set_ylim(configs['ylim'])
+            axFeature.set_ylabel(configs['label'], color=marker['color'])
+            axFeature.tick_params('y', colors=marker['color'])
+
+            lineFeature, = axFeature.plot(
+                df['millisecond'],
+                df[featureName],
+                **marker
+            )
+            lines += (lineFeature,)
+            labels += (featureName,)
+
+        fig.tight_layout()
+
+        # output figure
+        destDir = os.path.join(DEST_DIR, featurePlot)
+        if not os.path.exists(destDir):
+            os.makedirs(destDir)
+        title = '{0}_{1}'.format(
+            os.path.basename(filename),
+            '+'.join(featureNames)
+        )
+        plt.title(title)
+        plt.legend(lines, labels, loc='upper left')
+        plt.savefig(
+            os.path.join(destDir, '{0}.jpg'.format(title)),
+            dpi=400,
+            format='jpg'
+        )
+        plt.clf()
+
+def main():
+    filenames = glob.glob(os.path.join(SRC_DIR, '*.csv'))
+    for fileFullName in filenames:
+        filename = os.path.basename(fileFullName)
+        print('reading ' + filename + '...')
+
+        df = pd.read_csv(
+            fileFullName,
+            sep=',',
+            header=None,
+            names=INPUT_CSV_COLUMNS
+        )
+        offset = toMillisecond(df.iloc[0])
+        df['millisecond'] = toMillisecond(df) - offset
+        plot(df, filename)
+
+main()
