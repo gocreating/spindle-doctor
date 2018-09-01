88e0d20c5e3a23be6542530728f092022792773e
翁治平 <gocreating@gmail.com>
2018-09-01 20:09:30 +0800

    Update edge testing

--- src/test-classification-anomaly-detection-for-edge.py
+++ src/test-classification-anomaly-detection-for-edge.py
@@ -15,29 +15,24 @@ python test-classification-anomaly-detection-for-edge.py ^
     --src-breakpoint "../build/meta/phm2012/breakpoints-from-feature/breakpoint-256.csv"
 
 """
-import sys
 import os
 import time
-import math
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 from bisect import bisect_left
-from utils.utils import get_args
+from utils.utils import get_args, prepare_directory
 from utils.preprocess import get_windowed_data
 from utils.input import get_batch
-from models.EncDecEmbedding import Model
 import importlib
 
 p = importlib.import_module('extract-feature')
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 args = get_args()
-dataset = np.empty((0, args.step_size))
 
 def read_dataset():
-    global dataset
-
+    dataset = np.empty((0, args.step_size))
     df_chunks = pd.read_csv(
         args.test_src,
         header=None,
@@ -73,8 +68,9 @@ def read_dataset():
     if r != 0:
         dataset = dataset[0:-r]
 
+    return dataset
+
 if __name__ == '__main__':
-    read_dataset()
     tf.reset_default_graph()
     with tf.Graph().as_default():
         tf.set_random_seed(args.seed)
@@ -82,23 +78,31 @@ if __name__ == '__main__':
         importSaver = tf.train.import_meta_graph(os.path.join(args.src, '../model.meta'))
         importSaver.restore(sess, args.src)
 
-        for batch_idx in range(0, 1):
-            begin_idx = batch_idx * args.batch_size
-            end_idx = begin_idx + args.batch_size
-            xs = dataset[begin_idx: end_idx]
+        while True:
+            dataset = read_dataset()
+            for batch_idx in range(0, 1):
+                begin_idx = batch_idx * args.batch_size
+                end_idx = begin_idx + args.batch_size
+                xs = dataset[begin_idx: end_idx]
 
-            restored_predictions = sess.run('compute_cost/Reshape_1:0', feed_dict={
-                'input_layer/xs:0': xs,
-                'input_layer/ys:0': xs,
-                'input_layer/feed_previous:0': True,
-            })
-            restored_ys = sess.run('compute_cost/Reshape_3:0', feed_dict={
-                'input_layer/xs:0': xs,
-                'input_layer/ys:0': xs,
-                'input_layer/feed_previous:0': True,
-            })
-            mse = np.mean((restored_ys - restored_predictions) ** 2, axis=1)
-            is_anomaly = mse[0] > args.threshold
+                restored_predictions = sess.run('compute_cost/Reshape_1:0', feed_dict={
+                    'input_layer/xs:0': xs,
+                    'input_layer/ys:0': xs,
+                    'input_layer/feed_previous:0': True,
+                })
+                restored_ys = sess.run('compute_cost/Reshape_3:0', feed_dict={
+                    'input_layer/xs:0': xs,
+                    'input_layer/ys:0': xs,
+                    'input_layer/feed_previous:0': True,
+                })
+                mse = np.mean((restored_ys - restored_predictions) ** 2, axis=1)
+                is_anomaly = mse[0] > args.threshold
 
-            with open(os.path.join(args.src, '../../inference-result.txt'), 'w') as fd_result:
-                fd_result.write('anomaly' if is_anomaly else 'normal')
+                dest_dir = prepare_directory(os.path.join(
+                    args.src,
+                    '../../inference-result'
+                ))
+                with open(os.path.join(dest_dir, 'last.txt'), 'w') as fd_result:
+                    print('anomaly' if is_anomaly else 'normal')
+                    fd_result.write('anomaly' if is_anomaly else 'normal')
+            time.sleep(5)
